dc0d88d5569250d28be70f6de6e27976509221e9
chinmayirm <121074867+chinmayirm@users.noreply.github.com>
2025-01-28 22:18:01 +0530

    Add files via upload

---
+++ cart__init__.py
@@ -0,0 +1,43 @@
+import json
+import products
+from cart import dao
+from products import Product
+
+
+class Cart:
+    def __init__(self, id: int, username: str, contents: list[Product], cost: float):
+        self.id = id
+        self.username = username
+        self.contents = contents
+        self.cost = cost
+
+    @staticmethod
+    def load(data):
+        return Cart(data['id'], data['username'], data['contents'], data['cost'])
+
+
+def get_cart(username: str) -> list:
+    cart_details = dao.get_cart(username)
+    if not cart_details:  
+        return []
+    
+
+    items = [
+        product_id
+        for cart_detail in cart_details
+        for product_id in json.loads(cart_detail['contents'])  # Replaced eval with json.loads
+    ]
+    
+    return [products.get_product(product_id) for product_id in items]
+
+
+def add_to_cart(username: str, product_id: int):
+    dao.add_to_cart(username, product_id)
+
+
+def remove_from_cart(username: str, product_id: int):
+    dao.remove_from_cart(username, product_id)
+
+
+def delete_cart(username: str):
+    dao.delete_cart(username)
